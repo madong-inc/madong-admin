d7416d74fd5514334170ff6f17a18d8aea8f11be
Mr.April <liuguanqin.g@163.com>
2024-10-23 11:15:48 +0800

    【程序目录】

---
+++ app/admin/route/system.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *+------------------
+ * madong
+ *+------------------
+ * Copyright (c) https://gitee.com/motion-code  All rights reserved.
+ *+------------------
+ * Author: Mr. April (405784684@qq.com)
+ *+------------------
+ * Official Website: http://www.madong.cn
+ */
+
+use Webman\Route;
--- app/functions.php
+++ app/functions.php
@@ -2,3 +2,24 @@
 /**
  * Here is your custom functions.
  */
+
+/**
+ * 返回当前系统登录用户
+ *
+ * @param bool $returnFullInfo
+ *
+ * @return null
+ */
+//function getCurrentUser(bool $returnFullInfo = false)
+//{
+//    $request = request();
+//    if (!$request->hasMacro('adminInfo')) {
+//        return null;
+//    }
+//    $adminInfo = $request->adminInfo();
+//    if (!$adminInfo) {
+//        return null;
+//    }
+//    // 根据参数决定返回值
+//    return $returnFullInfo ? $adminInfo : $adminInfo['id'];
+//}
