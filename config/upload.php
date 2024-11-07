c17bfe6f45e22f6078c08b89780161214a543dce
Mr.April <liuguanqin.g@163.com>
2024-11-07 23:52:05 +0800

    feat: upload.php

---
+++ config/upload.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'debug'       => config('app.debug'),
+    'config_key'  => [
+        'local' => '',
+        'oss'   => '',
+        'cos'   => '',
+        'qiniu' => '',
+        's3'    => '',
+    ],
+    'task_handle' => [
+        'local' => '',
+        'oss'   => '',
+        'cos'   => '',
+        'qiniu' => '',
+        's3'    => '',
+    ],
+];
