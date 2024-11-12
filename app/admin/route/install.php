4c059c3b9ff0ac2faceb8dea697e5f3ea3d45b9e
Mr.April <liuguanqin.g@163.com>
2024-11-12 17:52:52 +0800

    feat: install 安装脚本

---
+++ app/admin/route/install.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *+------------------
+ * madong
+ *+------------------
+ * Copyright (c) https://gitee.com/motion-code  All rights reserved.
+ *+------------------
+ * Author: Mr. April (405784684@qq.com)
+ *+------------------
+ * Official Website: http://www.madong.tech
+ */
+
+use Webman\Route;
+
+Route::group('/install', function () {
+    Route::post('/step1', [\app\admin\controller\InstallController::class, 'step1'])->name('创建数据库');
+    Route::post('/step2', [\app\admin\controller\InstallController::class, 'step2'])->name('添加管理员');
+
+});
