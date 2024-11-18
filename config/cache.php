9b30c4a30f1566380a04d804dc1118c0c6441899
Mr.April <liuguanqin.g@163.com>
2024-11-18 23:43:28 +0800

    chore: cache.php

--- config/cache.php
+++ config/cache.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
-    'default'   => config('cache.redis'),
-    'namespace' => 'ma-dong',
-    'prefix'    => '',
-    'type'      => 'redis',//redis||file
-    'redis'     => [
+    'default'   => [
         'host'     => '127.0.0.1',
         'password' => null,
         'port'     => 6379,
         'database' => 0,
+    ],
+    'namespace' => 'ma-dong',
+    'prefix'    => '',
+    'type'      => 'redis',//redis||file
+    'store'     => [
+        //更多
+        'redis' => [],
+        'file'  => [],
     ],
 ];
