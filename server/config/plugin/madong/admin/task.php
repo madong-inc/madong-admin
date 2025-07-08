e17b2d947f77acf8e8aacd04734ff802fae5c096
Mr.April <liuguanqin.g@163.com>
2025-07-08 16:11:56 +0800

    chore: 修改端口号

--- server/config/plugin/madong/admin/task.php
+++ server/config/plugin/madong/admin/task.php
@@ -19,7 +19,7 @@ return [
     'debug'       => config('app.debug'),
     'enable'      => env('TASK_ENABLED',false),// 是否启用定时器  修改此参数后，需要重启
     'write_log'   => false,
-    'listen'      => '127.0.0.1:' . '2338',// 注意此端口用于任务通讯，一个项目一个端口，请勿占用
+    'listen'      => '127.0.0.1:' . '2337',// 注意此端口用于任务通讯，一个项目一个端口，请勿占用
     'task_handle' => [
         //任务操作类
         \app\common\enum\system\TaskScheduleType::TASK_URL->value      => UrlTask::class,
