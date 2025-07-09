1521d095850c631a136cd23646a4a68d600c5b66
Mr.April <liuguanqin.g@163.com>
2025-07-09 10:42:34 +0800

    feat: 修复兼容安装Mysql 5.7版本

--- server/scripts/sql/install-8.0.sql
+++ server/scripts/sql/install-8.0.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80012
  Source Host           : localhost:3306
- Source Schema         : madong_db1
+ Source Schema         : madong_db
 
  Target Server Type    : MySQL
  Target Server Version : 80012
