3648b6f5035b3c16d0c03cb9d8b88c52de4c908f
Mr.April <liuguanqin.g@163.com>
2025-07-14 11:19:10 +0800

    feat:添加支持回收站表配置

--- server/config/recycle_bin.php
+++ server/config/recycle_bin.php
@@ -18,19 +18,66 @@ return [
     'default' => [
         'enabled'      => true,       // 是否启用回收站
         'strategy'     => 'logical',  // logical|physical 默认软删
-        'storage_days' => 30,         // 保留天数
+        'storage_days' => 30,         // 保留天数-需自行扩展任务清理数据
         'storage_mode' => 'central',  // central|isolated (主库集中|分库隔离)
     ],
 
     // 表级配置覆盖
     'tables'  => [
-        'sys_upload' => [
+        /** 统一回收至主库 */
+        'sys_menu'               => [
             'strategy'     => 'physical',
-            'storage_mode' => 'isolated',//租户数据源
+            'storage_mode' => 'central',
         ],
-        'sys_menu'   => [
+        'sys_dict'               => [
             'strategy'     => 'physical',
-            'storage_mode' => 'central',//主库数据源
+            'storage_mode' => 'central',
+        ],
+        'sys_dict_item'          => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'sys_dept'               => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'sys_post'               => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'mt_db_setting'          => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'mt_tenant_subscription' => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'sys_role'               => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'sys_rate_limiter'       => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+        'sys_rate_restrictions'  => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'central',
+        ],
+
+        /** 统一回收至租户数据源 */
+        'sys_notice'             => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'isolated',
+        ],
+        'sys_upload'             => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'isolated',
+        ],
+        'sys_message'            => [
+            'strategy'     => 'physical',
+            'storage_mode' => 'isolated',
         ],
     ],
 ];
